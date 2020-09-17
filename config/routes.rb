7ad7f504cf4326899e92e7f368512d1bae771a0d
Jenny Citherlet <jennycith@gmail.com>
2020-09-17 22:11:02 +0000

    Fix route bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,30 @@
 Rails.application.routes.draw do
-
   root to: 'products#index'
-
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   match 'about' => 'about#show', :via => :get
-
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
   resources :orders, only: [:create, :show]
+   namespace :admin do
+     root to: 'dashboard#show'
+     resources :products, except: [:edit, :update, :show]
+     resources :categories, except: [:edit, :update, :show, :destroy]
+   end
 
-  namespace :admin do
-    root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show, :destroy]
-  end
-
-  # The priority is based upon order of creation: first created -> highest priority.
+   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
   # Example resource route with options:
   #   resources :products do
   #     member do
@@ -46,13 +36,11 @@ Rails.application.routes.draw do
   #       get 'sold'
   #     end
   #   end
-
   # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
   #   end
-
   # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
@@ -60,14 +48,12 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
   #   end
   #   resources :posts, concerns: :toggleable
   #   resources :photos, concerns: :toggleable
-
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
