97f38638b3304433f7a6dcd9c79ecc86d6557410
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-17 10:20:30 -0700

    Add feature to create new category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,9 +4,26 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def new
+    @category = Category.new
+  end
+
   def create
+    @category = Category.new(category_params)
+
+     if @category.save
+       redirect_to [:admin, :categories], notice: 'Category created!'
+     else
+       render :new
+     end
   end
 
   def new
+    private
+
+   def category_params
+     params.require(:category).permit(
+       :name
+     )
   end
 end
