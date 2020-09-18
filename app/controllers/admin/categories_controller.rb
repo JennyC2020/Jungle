2d26f51e20f24d0424b592044b0e1a289007d2d2
Jenny Citherlet <jcitherlet@cendyn.com>
2020-09-17 18:32:18 -0700

    Fix controller bug

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,7 +3,7 @@ class Admin::CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
-
+  
   def new
     @category = Category.new
   end
@@ -11,19 +11,19 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-     if @category.save
-       redirect_to [:admin, :categories], notice: 'Category created!'
-     else
-       render :new
-     end
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
-  def new
-    private
+  private
 
-   def category_params
-     params.require(:category).permit(
-       :name
-     )
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
+
 end
